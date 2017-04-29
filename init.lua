d736548969a09968057b04a6398548ebb250eb7f
AndrejIT <AndrejIT@rambler.ru>
2017-04-29 23:11:58 +0300

    Add code

---
+++ init.lua
@@ -0,0 +1,12 @@
+
+-- Just reciepe to create appletree sapling
+-- Maybe it was too expensive...
+
+minetest.register_craft({
+	output = "default:sapling",
+	recipe = {
+		{"", "", ""},
+		{"", "default:apple", ""},
+		{"", "default:dirt", ""},
+	},
+})
